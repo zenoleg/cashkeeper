ebb10b4af4a6958f4bb293d341526ac86bfe667a
Zenkovich <zenkovich@aeroidea.ru>
2019-05-12 19:41:37 +0300

    reformat routes

--- routes/botman.php
+++ routes/botman.php
@@ -1,16 +1,16 @@
 <?php
 
 use App\Http\Controllers\BotManController;
+use App\Http\Controllers\InitController;
 use BotMan\BotMan\BotMan;
 
 $botman = resolve('botman');
 
 $botman->hears('/start', 'App\Http\Controllers\InitController@start');
+$botman->hears('.*Баланс.*', InitController::class . '@balance');
 
 $botman->hears('Hi', function (BotMan $bot) {
     $bot->reply('Hello!');
 });
-
-$botman->hears('.*Баланс.*', 'App\Http\Controllers\InitController@balance');
 
 $botman->hears('Start conversation', BotManController::class . '@startConversation');
