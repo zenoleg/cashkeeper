77d56fe2b2bbac9199fd35b168377ed44baefccb
Zenkovich <zenkovich@aeroidea.ru>
2019-05-12 18:54:19 +0300

    move logic into controllers and services

---
+++ app/Http/Controllers/InitController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helpers\Keyboards\WelcomeKeyboard;
+use App\Helpers\Util;
+use App\Services\UserService;
+use BotMan\BotMan\BotMan;
+use Exception;
+use Illuminate\Http\Request;
+
+class InitController extends Controller
+{
+    public function start(BotMan $bot)
+    {
+        $welcomeKeyboard = new WelcomeKeyboard();
+        $userInfo = Util::getUserInfo($bot);
+
+        $userService = new UserService();
+        try {
+            $userService->initUser($bot);
+        } catch (Exception $e) {
+            $bot->reply($e->getMessage());
+        }
+
+        $bot->reply(sprintf("Добро пожаловать,\n%s", $userInfo['name']), $welcomeKeyboard->toArray());
+    }
+}
--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -4,24 +4,30 @@
 namespace App\Services;
 
 
+use App\Helpers\Util;
 use App\Models\User;
+use BotMan\BotMan\BotMan;
+use Exception;
 
 class UserService
 {
     /**
      * Инициализирует пользователя
      *
-     * @param int $userId
-     * @param string $name
-     * @throws \Exception
+     * @param BotMan $bot
+     * @throws Exception
      */
-    public function initUser(int $userId, string $name)
+    public function initUser(BotMan $bot)
     {
+        $userInfo = Util::getUserInfo($bot);
+        $userId = $userInfo['id'];
+        $userName = $userInfo['name'];
+
         if (!User::isExist($userId)) {
-            $isInit = User::initUser($userId, $name);
+            $isInit = User::initUser($userId, $userName);
 
             if (!$isInit) {
-                throw new \Exception('User can`t be inited', 500);
+                throw new Exception('User can`t be inited', 500);
             }
         }
     }
